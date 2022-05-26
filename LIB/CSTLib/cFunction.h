d3e2b38e23f0e81fe3d51a0862ef9a671434f18e
Inory <dawnbreakFallenSky@foxmail.com>
2022-05-26 10:59:13 +0800

    增加CSTLib库
    2022.5.26
    1.完成CSTLib库的编写，是C++标准库在C语言编程环境下的实现，方便C++在C语言编程环境的移植。具体包括mutex,condition_variable,thread,list,queue,map等。多数接口与C++标准库一致。
    by Inory

---
+++ LIB/CSTLib/cFunction.h
@@ -0,0 +1,80 @@
+﻿/*-----------------------------------------------------------------------------------------
+**
+** 版权(Copyright): Inory, 2022~
+**
+** 文件名(FileName): cFunction.h
+**
+** 描述(Description): 本文件定义函数(function)在C语言编译环境下的封装接口
+**
+** 设计注记(Design Annotation):
+**
+** 作者(Author):
+**      Inory, 2022年5月22日开始编写本文件
+**
+** 更改历史(Modification History):
+**      Inory, 2022年5月22日 创建本文件
+**      $Revision$
+**      $Date$
+**      $Author$
+**      $Log$
+**
+**-----------------------------------------------------------------------------------------
+*/
+#ifndef _C_FUNCTION_H
+#define _C_FUNCTION_H
+
+/*-----------------------------------------------------------------------------------------
+**                                         Include
+**-----------------------------------------------------------------------------------------
+*/
+#include <stdio.h>
+#include <string.h>
+#include "cPair.h"
+
+/*-----------------------------------------------------------------------------------------
+**                                     Type Declaration
+**-----------------------------------------------------------------------------------------
+*/
+/* 一元函数声明 */
+typedef struct CUnaryFunction CUnaryFunction;
+/* 二元函数声明 */
+typedef struct CBinaryFunction CBinaryFunction;
+
+/*-----------------------------------------------------------------------------------------
+**                                     Class Definition
+**-----------------------------------------------------------------------------------------
+*/
+/* <类描述> */
+/* 1)一元函数接口 */
+/* 2) */
+struct CUnaryFunction
+{
+	/* 函数 */
+	const void* (*O)(CUnaryFunction* thiz, const void* val);
+	/* 函数指针 */
+	void* _function;
+};
+
+/* <类描述> */
+/* 1)二元函数接口 */
+/* 2) */
+struct CBinaryFunction
+{
+	/* 函数 */
+	const void* (*O)(CBinaryFunction* thiz, const void* val1, const void* val2);
+	/* 函数指针 */
+	void* _function;
+};
+
+/*-----------------------------------------------------------------------------------------
+**                                   Function Declaration
+**-----------------------------------------------------------------------------------------
+*/
+/* 返回键值 */
+extern CUnaryFunction cFuncSelect1st(void);
+/* int型变量比较 */
+extern int intComparer(const void* x, const void* y);
+/* string型变量比较 */
+extern int strComparer(const void* x, const void* y);
+
+#endif /* _C_FUNCTION_H */
---
+++ LIB/CSTLib/cList.h
@@ -0,0 +1,86 @@
+﻿/*-----------------------------------------------------------------------------------------
+**
+** 版权(Copyright): Inory, 2022~
+**
+** 文件名(FileName): cList.h
+**
+** 描述(Description): 本文件定义列表(list)在C语言编译环境下的封装接口
+**
+** 设计注记(Design Annotation):
+**
+** 作者(Author):
+**      Inory, 2022年5月22日开始编写本文件
+**
+** 更改历史(Modification History):
+**      Inory, 2022年5月22日 创建本文件
+**      $Revision$
+**      $Date$
+**      $Author$
+**      $Log$
+**
+**-----------------------------------------------------------------------------------------
+*/
+#ifndef _C_LIST_H
+#define _C_LIST_H
+
+/*-----------------------------------------------------------------------------------------
+**                                         Include
+**-----------------------------------------------------------------------------------------
+*/
+#include "cIterator.h"
+
+/*-----------------------------------------------------------------------------------------
+**                                     Type Declaration
+**-----------------------------------------------------------------------------------------
+*/
+/* 列表结构体声明 */
+typedef struct CList CList;
+
+/*-----------------------------------------------------------------------------------------
+**                                     Class Definition
+**-----------------------------------------------------------------------------------------
+*/
+/* <类描述> */
+/* 1)列表接口 */
+/* 2) */
+struct CList
+{
+	/* 列表 */
+	void* _list;
+	/* 比较函数 */
+	comparer _keyCompare;
+	/* 节点中数据占用内存大小 */
+	int _nodeSize;
+
+	/* 返回列表首节点迭代器 */
+	CIterator (*begin)(CList* thiz);
+	/* 返回列表尾节点迭代器 */
+	CIterator (*end)(CList* thiz);
+	/* 在列表尾部插入节点 */
+	void (*push_back)(CList* thiz, const void* val);
+	/* 删除相邻的重复元素 */
+	void (*unique)(CList* thiz);
+};
+
+/*-----------------------------------------------------------------------------------------
+**                                   Function Declaration
+**-----------------------------------------------------------------------------------------
+*/
+/* 构造函数 */
+extern CList* CreateCList(int nodeSize, comparer keyCmp);
+/* 返回列表首节点迭代器 */
+extern CIterator cListBegin(CList* thiz);
+/* 返回列表末节点迭代器 */
+extern CIterator cListEnd(CList* thiz);
+/* 返回列表首节点的值 */
+extern void* cListFront(CList* thiz);
+/* 判断列表是否为空 */
+extern int cListEmpty(CList* thiz);
+/* 在列表末尾插入节点 */
+extern void cListPushBack(CList* thiz, const void* val);
+/* 在列表头部弹出节点 */
+extern void cListPopFront(CList* thiz);
+/* 删除重复相邻元素 */
+extern void cListUnique(CList* thiz);
+
+#endif /* _C_LIST_H */
